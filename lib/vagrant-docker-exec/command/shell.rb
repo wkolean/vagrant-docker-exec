3588a4944e46e201b677f02e2edc5ac352095008
William Kolean <william.kolean@rga.com>
2015-02-22 23:42:42 -0500

    added vagrant-shell command

--- lib/vagrant-docker-exec/command/shell.rb
+++ lib/vagrant-docker-exec/command/shell.rb
@@ -2,9 +2,37 @@ module VagrantPlugins
   module DockerExec
     class Command < Vagrant.plugin("2", "command")
       def execute
-        puts "docker-shell"
+
+        options = {}
+        options[:detach]  = false
+        options[:pty]     = true
+        options[:stdin]   = true
+
+        opts = OptionParser.new do |o|
+          o.banner = "Usage: vagrant docker-shell [container]"
+        end
+
+        # Parse the options
+        argv = parse_options(opts)
+        return if !argv
+
+        target_opts = { provider: :docker }
+        target_opts[:single_target] = options[:pty]
+
+        with_target_vms(argv, target_opts) do |machine|
+          command = ["docker", "exec", "-it"]
+          command << machine.name.to_s
+          command << "bash"
+          
+          output = ""
+          machine.provider.driver.execute(*command, options) do |type, data|
+            output += data
+          end
+        end
+
         return 0
       end
+
     end
   end
 end
